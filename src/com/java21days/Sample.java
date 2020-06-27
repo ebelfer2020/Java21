823fe2f9b671640cf073ae703f6be4a0268ee007
ebelfer2020 <60272644+ebelfer2020@users.noreply.github.com>
2020-06-27 15:21:07 -0600

    new commit

---
+++ src/com/java21days/Sample.java
@@ -0,0 +1,10 @@
+/* Solution to Chapter 3, Exercise 2 in Teach Yourself Java in 21 Days (Eighth
+   Edition) by Rogers Cadenhead. */
+package com.java21days;
+
+class Sample {
+    int height;
+    int weight;
+    int depth;
+}
+
